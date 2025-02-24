84fb61f8843653bbfa8b387abb3d87513f6aaa9e
isageorgete <isadorageorgete22@gmail.com>
2025-02-24 12:34:23 -0300

    Criação basíca dos dominios de entidades

---
+++ HelpApp/HelpApp.Domain/Entities/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpApp.Domain.Entities
+{
+    class Product
+    {
+        #region Atributos
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Image { get; set; }
+        public int CateegoryId { get; set; }
+        #endregion
+
+
+        #region Construtores
+        public Product(string name, string description, decimal price, int stock, string image)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            Stock = stock;
+            Image = image;
+
+        }
+         public Category Category { get; set; }
+        #endregion
+    }
+}
---
+++ HelpApp/HelpApp.Domain/Validation/DomainExcceptionValidation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpApp.Domain.Validation
+{
+    public class DomainExcceptionValidation: Exception
+    {
+        public DomainExcceptionValidation(string error) : base(error)
+        {
+
+        }
+        
+           public static void When (bool hasError, string error)
+        {
+            if (hasError)
+            {
+                throw new DomainExcceptionValidation(error);
+            }
+        }
+        
+
+        
+        
+    }
+}
